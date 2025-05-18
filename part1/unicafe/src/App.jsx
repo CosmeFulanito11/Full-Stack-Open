120e454fee189402ebd9e177ed86ae8c2de7fdec
LuisEKike <quiquin6606@gmail.com>
2025-05-18 10:59:12 -0600

    1.6 unicafe pt1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (type) => {
+    switch (type) {
+      case 'good':
+        setGood(good + 1)
+        break
+      case 'neutral':
+        setNeutral(neutral + 1)
+        break
+      case 'bad':
+        setBad(bad + 1)
+        break
+      default:
+        console.log('something went wrong')
+    }
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => handleClick('good')}>good</button>
+      <button onClick={() => handleClick('neutral')}>neutral</button>
+      <button onClick={() => handleClick('bad')}>bad</button>
+      <h1>statics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
